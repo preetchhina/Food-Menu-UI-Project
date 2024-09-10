5611c707973b09bac8868e6e4a9a504e67423d1b
Sahijpreet Chhina <150275940+preetchhina@users.noreply.github.com>
2024-09-09 22:53:03 -0400

    Update BudgetTracking.java

--- BudgetTracking.java
+++ BudgetTracking.java
@@ -1,28 +1,37 @@
-Label budgetLabel = new Label("Budget:");
-TextField budgetField = new TextField();
-Label totalCostLabel = new Label("Total Cost: 0");
-double[] totalCost = {0.0};
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
-Button checkBudgetButton = new Button("Check Budget");
+public class BudgetTrackingUI {
 
-gridPane.add(budgetLabel, 0, 4);
-gridPane.add(budgetField, 1, 4);
-gridPane.add(totalCostLabel, 0, 5);
-gridPane.add(checkBudgetButton, 1, 5);
+    private FoodMenuUIProject2024 app;
 
-addButton.setOnAction(e -> {
-    String name = nameField.getText();
-    double price = Double.parseDouble(priceField.getText());
-    int calories = Integer.parseInt(caloriesField.getText());
-    totalCost[0] += price;
-    totalCostLabel.setText("Total Cost: " + totalCost[0]);
-});
-
-checkBudgetButton.setOnAction(e -> {
-    double budget = Double.parseDouble(budgetField.getText());
-    if (totalCost[0] > budget) {
-        System.out.println("Over budget!");
-    } else {
-        System.out.println("Within budget.");
+    public BudgetTrackingUI(FoodMenuUIProject2024 app) {
+        this.app = app;
     }
-});
+
+    public void addBudgetTrackingSection(GridPane gridPane) {
+        Label budgetLabel = new Label("Budget:");
+        TextField budgetField = new TextField();
+        Label totalCostLabel = new Label("Total Cost: 0");
+        Button checkBudgetButton = new Button("Check Budget");
+
+        gridPane.add(budgetLabel, 0, 4);
+        gridPane.add(budgetField, 1, 4);
+        gridPane.add(totalCostLabel, 0, 5);
+        gridPane.add(checkBudgetButton, 1, 5);
+
+        checkBudgetButton.setOnAction(e -> {
+            double totalCost = 0;
+            for (int i = 0; i < app.itemCount; i++) {
+                totalCost += app.itemPrices[i];
+            }
+            totalCostLabel.setText("Total Cost: " + totalCost);
+            app.budget = Double.parseDouble(budgetField.getText());
+            if (totalCost > app.budget) {
+                System.out.println("Over budget!");
+            } else {
+                System.out.println("Within budget.");
+            }
+        });
+    }
+}
