f2d7c006b746e1ca029d69aff9fa49e94e508337
Sahijpreet Chhina <150275940+preetchhina@users.noreply.github.com>
2024-09-09 22:54:03 -0400

    Update MealPlanningSystem.java

--- MealPlanningSystem.java
+++ MealPlanningSystem.java
@@ -1,14 +1,27 @@
-Label mealPlanLabel = new Label("Select meals for Monday:");
-Button breakfastButton = new Button("Add to Breakfast");
-Button lunchButton = new Button("Add to Lunch");
-Button dinnerButton = new Button("Add to Dinner");
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
-gridPane.add(mealPlanLabel, 0, 8);
-gridPane.add(breakfastButton, 0, 9);
-gridPane.add(lunchButton, 1, 9);
-gridPane.add(dinnerButton, 2, 9);
+public class MealPlanningUI {
 
-breakfastButton.setOnAction(e -> {
-    // Add the current selected item to the breakfast plan
-    System.out.println("Added to Monday Breakfast");
-});
+    private FoodMenuUIProject2024 app;
+
+    public MealPlanningUI(FoodMenuUIProject2024 app) {
+        this.app = app;
+    }
+
+    public void addMealPlanningSection(GridPane gridPane) {
+        Label mealPlanLabel = new Label("Select meals for the week:");
+        Button breakfastButton = new Button("Add to Breakfast");
+        Button lunchButton = new Button("Add to Lunch");
+        Button dinnerButton = new Button("Add to Dinner");
+
+        gridPane.add(mealPlanLabel, 0, 9);
+        gridPane.add(breakfastButton, 0, 10);
+        gridPane.add(lunchButton, 1, 10);
+        gridPane.add(dinnerButton, 2, 10);
+
+        breakfastButton.setOnAction(e -> {
+            System.out.println("Added to Breakfast Plan");
+        });
+
+        lunchButton.set
