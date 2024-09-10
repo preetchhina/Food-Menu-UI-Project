5342b7b49d59ef98e67d433601d7c08eb24074c0
Sahijpreet Chhina <150275940+preetchhina@users.noreply.github.com>
2024-09-09 22:53:30 -0400

    Update CalorieTracking.java

--- CalorieTracking.java
+++ CalorieTracking.java
@@ -1,16 +1,37 @@
-Label calorieLimitLabel = new Label("Calorie Limit:");
-TextField calorieLimitField = new TextField();
-Label totalCaloriesLabel = new Label("Total Calories: 0");
-int[] totalCalories = {0};
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
-gridPane.add(calorieLimitLabel, 0, 6);
-gridPane.add(calorieLimitField, 1, 6);
-gridPane.add(totalCaloriesLabel, 0, 7);
+public class CalorieTrackingUI {
 
-addButton.setOnAction(e -> {
-    String name = nameField.getText();
-    double price = Double.parseDouble(priceField.getText());
-    int calories = Integer.parseInt(caloriesField.getText());
-    totalCalories[0] += calories;
-    totalCaloriesLabel.setText("Total Calories: " + totalCalories[0]);
-});
+    private FoodMenuUIProject2024 app;
+
+    public CalorieTrackingUI(FoodMenuUIProject2024 app) {
+        this.app = app;
+    }
+
+    public void addCalorieTrackingSection(GridPane gridPane) {
+        Label calorieLimitLabel = new Label("Calorie Limit:");
+        TextField calorieLimitField = new TextField();
+        Label totalCaloriesLabel = new Label("Total Calories: 0");
+        Button checkCaloriesButton = new Button("Check Calories");
+
+        gridPane.add(calorieLimitLabel, 0, 6);
+        gridPane.add(calorieLimitField, 1, 6);
+        gridPane.add(totalCaloriesLabel, 0, 7);
+        gridPane.add(checkCaloriesButton, 1, 7);
+
+        checkCaloriesButton.setOnAction(e -> {
+            int totalCalories = 0;
+            for (int i = 0; i < app.itemCount; i++) {
+                totalCalories += app.itemCalories[i];
+            }
+            totalCaloriesLabel.setText("Total Calories: " + totalCalories);
+            app.calorieLimit = Integer.parseInt(calorieLimitField.getText());
+            if (totalCalories > app.calorieLimit) {
+                System.out.println("Over calorie limit!");
+            } else {
+                System.out.println("Within calorie limit.");
+            }
+        });
+    }
+}
